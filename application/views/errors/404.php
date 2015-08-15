00cabc5c784900abcafd9171b6f6c4feb8a48633
Gabriel Ignisca <madalin.ignisca@gmail.com>
2015-08-15 09:42:46 +0100

    Virtualbox gives some troubles from time to time when moving shared files or folders

---
+++ application/views/errors/404.php
@@ -0,0 +1,9 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+/**
+ * Created by PhpStorm.
+ * User: madalin
+ * Date: 15/08/2015
+ * Time: 09:31
+ */ ?>
+
+<h1>The page requested does not exist or it was lost in the Ocean!</h1>
